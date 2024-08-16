69dbc0f1989fd6fb10055a78c8971ab5f4e0182d
xaniel123 <xanielmukkala@gmail.com>
2024-08-16 16:51:11 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+My-Linux-Setup-Script
+This is a tool I made to set up a fresh debian based Linux install on my Framwork with the tools I use.
---
+++ Setup.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+RED='\033[0;31m';
+NC='\033[0m'; # No Color
+GREEN='\033[0;32m';
+YELLOW='\033[1;33m';
+
+CWD=`pwd`;
+
+delay_after_message=3;
+
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run with sudo (sudo -i)" 
+   exit 1
+fi
+
+read -p "Please enter your username: " target_user;
+
+if id -u "$target_user" >/dev/null 2>&1; then
+    echo "User $target_user exists! Proceeding.. ";
+else
+    echo 'The username you entered does not seem to exist.';
+    exit 1;
+fi
+
+# Function to run as a non-root user
+run_as_user() {
+	sudo -u $target_user bash -c "$1";
+}
+
+apt update
+
+# Remove Firefox snap and set up apt repository for firefox.
+printf "${YELLOW}Removing Firefox snap and setting up Firefox apt repository${NC}\n"
+sleep $delay_after_message;
+snap remove firefox
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+# Adds the 1password apt Repository
+printf "${YELLOW} Adding the 1password apt Repository${NC}\n"
+sleep $delay_after_message;
+sudo -s \
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+tee /etc/apt/sources.list.d/1password.list
+mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+# Adds the Cloudflare Warp Repositories
+printf "${YELLOW} Adding the Cloudflare Warp Repositories apt Repository${NC}\n"
+sleep $delay_after_message;
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
+
+# Adds the Spotify APT Repository
+printf "${YELLOW} Adding the Spotify apt Repository${NC}\n"
+sleep $delay_after_message;
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+
+printf "${YELLOW} Updating APT and installing packages${NC}\n"
+sleep $delay_after_message;
+apt update
+for i in $(cat pkglist);
+    do sudo apt install $i;
+done
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.prismlauncher.PrismLauncher
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub tv.plex.PlexDesktop
+flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community
+
+printf "${RED}Removing thunderbird completely${NC}\n";
+sleep $delay_after_message;
+apt-get purge thunderbird* -y
+
+printf "${YELLOW}Setting up Ubuntu to work with the framework laptop${NC}\n"
+sleep $delay_after_message;
+apt install linux-oem-22.04d-y
+sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash module_blacklist=hid_sensor_hub"/g' /etc/default/grub && latest_oem_kernel=$(ls /boot/vmlinuz-* | grep '6.5.0-10..-oem' | sort -V | tail -n1 | awk -F'/' '{print $NF}' | sed 's/vmlinuz-//') && sudo sed -i.bak '/^GRUB_DEFAULT=/c\GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux '"$latest_oem_kernel"'"' /etc/default/grub && sudo update-grub && sudo apt install zenity && mkdir -p ~/.config/autostart && [ ! -f ~/.config/autostart/kernel_check.desktop ] && echo -e "[Desktop Entry]\nType=Application\nExec=bash -c \"latest_oem_kernel=\$(ls /boot/vmlinuz-* | grep '6.5.0-10..-oem' | sort -V | tail -n1 | awk -F'/' '{print \\\$NF}' | sed 's/vmlinuz-//') && current_grub_kernel=\$(grep '^GRUB_DEFAULT=' /etc/default/grub | sed -e 's/GRUB_DEFAULT=\\\"Advanced options for Ubuntu>Ubuntu, with Linux //g' -e 's/\\\"//g') && [ \\\"\\\${latest_oem_kernel}\\\" != \\\"\\\${current_grub_kernel}\\\" ] && zenity --text-info --html --width=300 --height=200 --title=\\\"Kernel Update Notification\\\" --filename=<(echo -e \\\"A newer OEM D kernel is available than what is set in GRUB. <a href='https://github.com/FrameworkComputer/linux-docs/blob/main/22.04-OEM-D.md'>Click here</a> to learn more.\\\")\"\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=Kernel check\nName=Kernel check\nComment[en_US]=\nComment=" > ~/.config/autostart/kernel_check.desktop
+
+
+
