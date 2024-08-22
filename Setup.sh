a5f1fa4f80fc784df1ed451e440c785c5594284f
xaniel123 <xanielmukkala@gmail.com>
2024-08-22 16:49:41 -0400

    modified:   Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -46,17 +46,12 @@ Pin-Priority: 1000
 # Adds the 1password apt Repository
 printf "${YELLOW} Adding the 1password apt Repository${NC}\n"
 sleep $delay_after_message;
-sudo -s \
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
-tee /etc/apt/sources.list.d/1password.list
-mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
 # Adds the Cloudflare Warp Repositories
 printf "${YELLOW} Adding the Cloudflare Warp Repositories apt Repository${NC}\n"
@@ -74,7 +69,7 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 printf "${YELLOW} Updating APT and installing packages${NC}\n"
 sleep $delay_after_message;
 apt update
-for i in $(cat pkglist);
+for i in $(cat APT_PACKAGE_LIST);
     do 
     printf "${YELLOW} Installing $i"
     sudo apt install $i;
